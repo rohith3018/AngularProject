e36c33d8e8959db653e7682eb71bfa4327d79fb8
rohith3018 <rohithjupally18@gmail.com>
2025-09-16 22:35:48 -0500

    Add files via upload

---
+++ services/src/app/new-account/new-account.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { AccountsService } from '../accounts.service';
+import { LoggingService } from '../logging.service';
+
+@Component({
+  selector: 'app-new-account',
+  templateUrl: './new-account.component.html',
+  styleUrls: ['./new-account.component.css'],
+ // providers:[LoggingService]
+})
+export class NewAccountComponent {
+  
+  constructor(private loggingService:LoggingService,private accountService:AccountsService){
+    this.accountService.statusUpdated.subscribe(
+      (status:string)=>alert('New Status:'+status)
+    );
+  }
+
+  onCreateAccount(accountName: string, accountStatus: string) {
+   this.accountService.addAccount(accountName,accountStatus)
+   //this.loggingService.logStatusChange(accountStatus);
+  }
+}
